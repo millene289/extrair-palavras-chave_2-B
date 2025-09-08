a6d8bdb3db4759b495cd10b1683530e39061f22a
millene289 <millene.daniel.mendonca@escola.pr.gov.br>
2025-09-08 08:00:20 -0300

    aula 0809

---
+++ script.js
@@ -0,0 +1,13 @@
+ import {PLAVRAS_RUINS} from "./palavrasruins.js";
+
+ const BotaoMostraPalavras = document.querySelector ('#botao-palavrachave');
+
+BotaoMostraPalavras.addEventListener ('click',MostraPalavrasChave);
+
+function MostraPalavrasChave () {
+ const texto = document.querySelector ('entrada-de-texto').value;
+ const campoRsultade=document.querySelector('#resultado-palavrachave');
+ const palavraChave=processaTexto(texto);
+
+  campoResultado.textContent=palavraChave.join(",");
+}
