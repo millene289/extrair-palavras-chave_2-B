f9df2cc1e2433073334224795058d6202bd24135
millene289 <millene.daniel.mendonca@escola.pr.gov.br>
2025-09-22 07:39:05 -0300

    aula 22/09

--- script.js
+++ script.js
@@ -21,12 +21,25 @@ function MostraPalavrasChave () {
 
    palavras = tirapalavrasRuins(palavras);
 
-   const frequencias = contaFrequencia(palavras);
-   let ordenadas = Object.keys(frequencias).short(ordenaPalavra);
+   const frequencias = contaFrequencias(palavras);
+   let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-     function ordenaPalavra(p1.p2){
+     function ordenaPalavra(p1,p2){
       return frequencias[p2] - frequencias[p1]
      }
   
-  
-    }
+   return ordenadas.slice(0,10);
+     }
+
+    function contaFrequenecia(palavras) {
+       let frequencias = {};
+       for (let i of palavras) {
+        frequencias[i]=0;
+        for (let j of palavras) {
+          if (i == j) {
+            frequencias [i] ++;
+          }
+        }
+      }
+    return frequencias;
+ }
